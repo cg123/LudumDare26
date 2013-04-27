10a4d744db607fd78695c4f1fd46d2a49f57809b
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-27 15:14:45 -0400

    Trying to make rotation work.

--- ld26/Assets/AvatarController.cs
+++ ld26/Assets/AvatarController.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
 using System.Collections;
 
-public class AvatarController : OVRPlayerController {
+public class AvatarController : MonoBehaviour {
 
 	// Initial direction of controller (passed down into CameraController)
 	//private Quaternion OrientationOffset = Quaternion.identity;			
 	// Rotation amount from inputs (passed down into CameraController)
 	//private float YRotation = 0.0f;
 
-	public GameObject fwdDir = null;
+	//public GameObject fwdDir = null;
 	public float moveSpeed = 0.0f;
 	public float turnSpeed = 0.0f;
 	private CharacterController charController = null;
 	private OVRCameraController camController = null;
+	private float yRotOffset = 0.0f;
 
-	new void Awake () {
-		base.Awake();
+	void Awake () {
 		charController = GetComponent<CharacterController>();
+		camController = GetComponentInChildren<OVRCameraController>();
 	}
 
-	new void Update () {
-		base.Update();
+	void Update () {
 		//float rot = 0.0f;
 		//camController.SetYRotation(0.0f);
 		//Debug.Log(rot);
@@ -28,44 +28,11 @@ public class AvatarController : OVRPlayerController {
 		//transform.eulerAngles = currAngle;
 		float fwd = Input.GetAxis ("Vertical") * moveSpeed;
 		float turn = Input.GetAxis ("Horizontal") * turnSpeed;
-		charController.Move(fwdDir.transform.forward * fwd * Time.deltaTime);
+		charController.Move(transform.forward * fwd * Time.deltaTime);
+		
+		transform.eulerAngles = camController.gameObject.transform.localRotation.eulerAngles + new Vector3(0.0f,yRotOffset,0.0f);
 		//transform.position += transform.forward * fwd * Time.deltaTime;
-		fwdDir.transform.Rotate(Vector3.up * turn * Time.deltaTime);
+		yRotOffset += turn * Time.deltaTime;
+		//fwdDir.transform.Rotate(Vector3.up * );
 	}
-
-
-	/*
-
-	void Start () {
-		SetCameras();
-		InitializeInputs();
-	}
-
-	// Use this for initialization
-
-
-	void OnDrawGizmos () {
-		Gizmos.DrawWireSphere(transform.position, 1.0f);
-		Gizmos.color = Color.red;
-		Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
-	}
-
-	// InitializeInputs
-	public void InitializeInputs () {
-		// Get our start direction
-		OrientationOffset = transform.rotation;
-		// Make sure to set y rotation to 0 degrees
-		YRotation = 0.0f;
-	}
-
-	// SetCameras
-	public void SetCameras () {
-		if(camController != null) {
-			// Make sure to set the initial direction of the camera 
-			// to match the game player direction
-			camController.SetOrientationOffset(OrientationOffset);
-			camController.SetYRotation(YRotation);
-		}
-	}*/
-
 }
