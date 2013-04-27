e9a824618dac340e5e54ea7a679cd9ed5f908e98
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-27 15:33:38 -0400

    Cleaned up code.

--- ld26/Assets/AvatarController.cs
+++ ld26/Assets/AvatarController.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public class AvatarController : MonoBehaviour {
-
-	// Initial direction of controller (passed down into CameraController)
-	//private Quaternion OrientationOffset = Quaternion.identity;			
-	// Rotation amount from inputs (passed down into CameraController)
-	//private float YRotation = 0.0f;
-
 	public GameObject fwdDir = null;
 	public float moveSpeed = 0.0f;
 	public float turnSpeed = 0.0f;
@@ -21,19 +15,12 @@ public class AvatarController : MonoBehaviour {
 	}
 
 	void Update () {
-		//float rot = 0.0f;
-		//camController.SetYRotation(0.0f);
-		//Debug.Log(rot);
-		//Vector3 currAngle = camController.transform.eulerAngles;
-		//transform.eulerAngles = currAngle;
 		float fwd = Input.GetAxis ("Vertical") * moveSpeed;
 		float turn = Input.GetAxis ("Horizontal") * turnSpeed;
 		charController.Move(fwdDir.transform.forward * fwd * Time.deltaTime);
 		
-		fwdDir.transform.eulerAngles = camController.gameObject.transform.eulerAngles;// + new Vector3(0.0f,yRotOffset,0.0f);
+		fwdDir.transform.rotation = camController.gameObject.transform.rotation;
 		camController.SetYRotation(yRotOffset);
-		//transform.position += transform.forward * fwd * Time.deltaTime;
 		yRotOffset += turn * Time.deltaTime;
-		//fwdDir.transform.Rotate(Vector3.up * );
 	}
 }
